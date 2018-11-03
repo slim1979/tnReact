33fe955de98f412e5aaa5b9fbadebf62ce4fbf46
slim1979 <chinezan@yandex.ru>
2018-11-03 20:25:57 +0300

    added arithmetic actions

---
+++ src/Arithmetic.js
@@ -0,0 +1,17 @@
+export default class Count {
+  static summ(f, s) {
+    return f + s
+  }
+
+  static subtraction(f, s) {
+    return f - s
+  }
+
+  static multiply(f, s) {
+    return f * s
+  }
+
+  static division(f, s) {
+    return (f / s).toFixed(4)
+  }
+}
